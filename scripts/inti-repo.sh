1eda8d622046c1ed7b579c365b3bf1388d5cae04
Wallace Breza <wallace.breza@microsoft.com>
2021-11-15 11:34:47 -0800

    Test

---
+++ scripts/inti-repo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eu
+parent_path=$(
+    cd "$(dirname "${BASH_SOURCE[0]}")"
+    pwd -P
+)
+cd "$parent_path"
+
+echo -n 'Creating python virtual environment...'
+python -m venv ../venv
+. ../venv/Scripts/activate
+echo 'done'
+
+echo -n 'Installing repo dependencies...'
+pip install -r ../requirements.txt
+echo 'done'
+
+echo -n 'Running pre-commit hooks for first time setup'
+pre-commit install
+#rm ./.git/hooks/pre-commit.legacy
+pre-commit run --all-files
+eccho 'done'
